264e25a37831ebb247a13c8e2636a851a295b8ef
Mostafa Mahmoud Salem <mostafamahmoud07uv@gmail.com>
2024-02-17 16:39:26 +0000

    solved isPasswordComplex problem

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -5,7 +5,24 @@ import java.util.Scanner;
 public class App {
 
     public static boolean isPasswordComplex(String password) {
-        return false;
+        if (null == password || password.length() < 6) {
+            return false;
+        }
+
+        boolean hasNumber = false, hasLower = false, hasUpper = false;
+        for (int i = 0, len = password.length(); i < len; ++i) {
+            char c = password.charAt(i);
+            if (Character.isDigit(c)) {
+                hasNumber = true;
+            }
+            if (Character.isLowerCase(c)) {
+                hasLower = true;
+            }
+            if (Character.isUpperCase(c)) {
+                hasUpper = true;
+            }
+        }
+        return hasNumber && hasLower && hasUpper;
     }
 
     public static void main(String[] args) {
